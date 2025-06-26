7396c6dfa13ff30f01a5d1f1e9762d5e879ce699
maiquelb <maiquelb@gmail.com>
2025-06-26 15:59:27 -0300

    update j48 internal action

--- jason_j48/src/java/jason/stdlib/j48.java
+++ jason_j48/src/java/jason/stdlib/j48.java
@@ -7,6 +7,7 @@ import jason.asSyntax.ListTermImpl;
 import jason.asSyntax.NumberTermImpl;
 import jason.asSyntax.Term;
 import jason.asSyntax.Literal;
+import jason.asSyntax.Rule;
 
 import static jason.asSyntax.ASSyntax.createAtom;
 import static jason.asSyntax.ASSyntax.parseLiteral;
@@ -20,15 +21,29 @@ import java.text.ParseException;
 import java.util.*;
 import java.util.regex.*;
 
+import rule2formula.DataSet;
+import rule2formula.DecisionRule;
+
+
 public class j48 extends DefaultInternalAction {
 
         @Override
         public Object execute(TransitionSystem ts, Unifier un, Term[] args) throws Exception {
-            ListTermImpl rules = run_j48(args[0].toString().replaceAll("^\"|\"$", ""));      
-            return un.unifies(rules, args[1]);
+            DataSet rules = run_j48(args[0].toString().replaceAll("^\"|\"$", ""));     
+            ListTermImpl listRules = new ListTermImpl();
+            Iterator<DecisionRule> it = rules.getRules().iterator();
+            while(it.hasNext()){
+                Rule r = it.next().toJasonLogicalFormula();
+                listRules.add(r);
+                ts.getAg().addBel(r);
+                //System.out.println(it.next()); 
+                //ts.getAg().addBel(it.next().toJasonLogicalFormula());
+            }
+            return un.unifies(listRules, args[1]);
+            //return true;
         }
 
-        private ListTermImpl run_j48(String datasetFileName){
+        private DataSet run_j48(String datasetFileName){
             try {
                DataSource source = new DataSource(datasetFileName); // load the dataset
                Instances dataset = source.getDataSet();
@@ -50,8 +65,22 @@ public class j48 extends DefaultInternalAction {
                eval.crossValidateModel(arvore, dataset, 10, new Random(1));
 
 
+                
+                System.out.println(arvore.toString());
+
+                DataSet ds = new DataSet();
+                if(ds.getFromWekaDecisionTree(arvore.toString(),dataset.classAttribute().name())){
+                   //System.out.println(ds);
+                //    Iterator<DecisionRule> it = ds.getRules().iterator();
+                //    while(it.hasNext())
+                //       System.out.println(it.next().toJasonLogicalFormula());
+                    return ds;
+                }
+
                //format rules to a suitable representation for Jason
-               return  extrairRegras(arvore.toString(),dataset.classAttribute().name());
+               //return  extrairRegras(arvore.toString(),dataset.classAttribute().name());
+               
+               
         
             }catch (Exception e) {
                // TODO Auto-generated catch block
@@ -103,9 +132,9 @@ public class j48 extends DefaultInternalAction {
          */
         public static ListTermImpl extrairRegras(String treeStr, String className) {
             
-
-            ListTermImpl listConditions = new ListTermImpl(); //list of conditions - managed as a stack (LIFO policy)
             ListTermImpl listResult = new ListTermImpl(); //list of rules (to be returned by this method)
+            ListTermImpl listConditions = new ListTermImpl(); //list of conditions - managed as a stack (LIFO policy)
+            
 
             List<String> regras = new ArrayList<>();
 
@@ -116,6 +145,7 @@ public class j48 extends DefaultInternalAction {
             Pattern linhaFolha = Pattern.compile(".*: ([^ ]+) \\((\\d+\\.?\\d*)(/\\d+\\.?\\d*)?\\)");
     
             for (String linha : linhas) { //for each line of the input tree
+                System.out.println("---> " + linha);
                 
 
                 if (!linha.contains(":") && !linha.contains("=")) continue;
@@ -129,35 +159,35 @@ public class j48 extends DefaultInternalAction {
                 }
     
                 if (linhaFolha.matcher(linha).matches()) {
-                    // Linha com classe (folha)
-                    Matcher m = linhaFolha.matcher(linha);
-                    if (m.find()) {
-                        String classe = m.group(1);
-                        String peso = m.group(2);
+            //         // Linha com classe (folha)
+                     Matcher m = linhaFolha.matcher(linha);
+            //         if (m.find()) {
+            //             String classe = m.group(1);
+            //             String peso = m.group(2);
     
     
-                        // Remove condição final do tipo: windy = TRUE: no (2.0)
-                        String[] partes = condicaoOuClasse.split(":");
-                        String cond = partes[0].trim();
-                        caminhoAtual.push(formatarCond(cond));
-                        listConditions.add(condition2Term(formatarCond(cond)));
+            //             // Remove condição final do tipo: windy = TRUE: no (2.0)
+            //             String[] partes = condicaoOuClasse.split(":");
+            //             String cond = partes[0].trim();
+            //             caminhoAtual.push(formatarCond(cond));
+            //             listConditions.add(condition2Term(formatarCond(cond)));
 
-                        ListTermImpl listRule = new ListTermImpl();
-                        listRule.add(listConditions.clone());
-                        listRule.add(condition2Term(String.format("result(%s,%s,%s)",  className, classe, peso)));      
-                        listResult.add(listRule);                  
+            //             ListTermImpl listRule = new ListTermImpl();
+            //             listRule.add(listConditions.clone());
+            //             listRule.add(condition2Term(String.format("result(%s,%s,%s)",  className, classe, peso)));      
+            //             listResult.add(listRule);                  
 
-                        listConditions.remove(listConditions.size()-1);
-                        caminhoAtual.pop(); // Remove condição final
-                    }
+            //             listConditions.remove(listConditions.size()-1);
+            //             caminhoAtual.pop(); // Remove condição final
+            //         }
                 } else {
                     // Condição intermediária, sem o '|'
-                    if(!condicaoOuClasse.contains("Number of Leaves")&&!condicaoOuClasse.contains("Size of the tree")){
-                       condicaoOuClasse = condicaoOuClasse.replace("|   ", "").trim(); // Remove o '|' e o espaço
-                       caminhoAtual.push(formatarCond(condicaoOuClasse));
-                       listConditions.add(condition2Term(formatarCond(condicaoOuClasse)));
+                    // if(!condicaoOuClasse.contains("Number of Leaves")&&!condicaoOuClasse.contains("Size of the tree")){
+                    //    condicaoOuClasse = condicaoOuClasse.replace("|   ", "").trim(); // Remove o '|' e o espaço
+                    //    caminhoAtual.push(formatarCond(condicaoOuClasse));
+                    //    listConditions.add(condition2Term(formatarCond(condicaoOuClasse)));
 
-                    }
+                    // }
                 }
                 
             }
